241d16589a14520a6bf28e339216bedd3ed877d6
Matthias Herzog <herzogmatthias@gmx.at>
2021-03-19 12:49:51 +0100

    added some basic settings

--- src/commands/codeToPdf.ts
+++ src/commands/codeToPdf.ts
@@ -8,9 +8,19 @@ import { TreeNode } from "../models/treeNode";
 import { buildTree } from "../utils/buildTree";
 
 export async function printDefinitionsForActiveEditor(uri: vscode.Uri) {
+  const openHTMLPage = vscode.workspace
+    .getConfiguration()
+    .get("codeToPdf.openHTMLPageInBrowser");
   console.log(uri.fsPath);
   let isDir = statSync(uri.fsPath).isDirectory();
+  let downloadDir = isDir
+    ? uri.fsPath
+    : join(...uri.path.split("/").splice(0, uri.path.split("/").length - 1));
   let tree: TreeNode | undefined = undefined;
+  let defaultName: string = isDir
+    ? uri.path.split("/")[uri.path.split("/").length - 1]
+    : uri.path.split("/")[uri.path.split("/").length - 1].split(".")[0];
+  defaultName += "_code.pdf";
   let flattendArray: Array<TreeNode> = [];
   if (isDir) {
     tree = await buildTree(uri.fsPath);
@@ -26,7 +36,7 @@ export async function printDefinitionsForActiveEditor(uri: vscode.Uri) {
     );
   }
   console.log(tree);
-  createServer(function (req, res) {
+  const server = createServer(function (req, res) {
     console.log(req.url);
     switch (req.url) {
       case "/":
@@ -57,8 +67,18 @@ export async function printDefinitionsForActiveEditor(uri: vscode.Uri) {
       default:
         break;
     }
-  }).listen(8080);
-  vscode.env.openExternal(vscode.Uri.parse("http://localhost:8080"));
+  }).listen(0);
 
-  await convertHtmlToPdf("http://localhost:8080");
+  if (openHTMLPage) {
+    vscode.env.openExternal(
+      vscode.Uri.parse(`http://localhost:${(server.address()! as any).port}`)
+    );
+  }
+
+  await convertHtmlToPdf(
+    `http://localhost:${(server.address()! as any).port}`,
+    downloadDir,
+    defaultName
+  );
+  server.close();
 }
--- src/utils/convertHtmlToPdf.ts
+++ src/utils/convertHtmlToPdf.ts
@@ -1,17 +1,18 @@
 import { join } from "path";
 import { launch } from "puppeteer";
 
-export async function convertHtmlToPdf(url: string) {
+export async function convertHtmlToPdf(
+  url: string,
+  downloadDir: string,
+  defaultName: string
+) {
   const browser = await launch({ headless: true });
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: "networkidle0" });
   console.log(process.env);
+  console.log(downloadDir);
   await page.pdf({
-    path: join(
-      process.env.HOME || process.env.USERPROFILE || "",
-      "downloads",
-      "code.pdf"
-    ),
+    path: join(downloadDir, defaultName),
     format: "a3",
   });
   browser.close();
