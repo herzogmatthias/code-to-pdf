dcab7672877a44cb8249c62c8aabb9d84997d7f7
Matthias Herzog <herzogmatthias@gmx.at>
2021-04-07 14:57:31 +0200

    changed to puppeteer-core to be more lightweigth, only usage of chrome is supported firefox has various issues

--- src/utils/convertHtmlToPdf.ts
+++ src/utils/convertHtmlToPdf.ts
@@ -1,29 +1,55 @@
 import { writeFileSync } from "fs";
-import { launch } from "puppeteer";
+import { launch } from "puppeteer-core";
 import { window } from "vscode";
-
+import { getOSSpecificPath } from "./getOSSpecificPath";
 export async function convertHtmlToPdf(url: string) {
-  const browser = await launch({ headless: true });
+  const path = await getOSSpecificPath();
+  if (!path) {
+    window.showErrorMessage(
+      "You do not have the necassary Browser (please use Firefox or Chrome!)"
+    );
+    return;
+  }
+  const browser = await launch({
+    headless: false,
+
+    executablePath: path,
+    args: ["-wait-for-browser"],
+    product: path.toLowerCase().includes("firefox") ? "firefox" : "chrome",
+  });
   const page = await browser.newPage();
   console.log("converting");
-  await page.goto(url, { waitUntil: "networkidle0" });
+  await page.goto(url);
+  try {
+    await page.waitForTimeout(500);
+  } catch (e) {
+    console.error(e);
+    window.showErrorMessage("Something went wrong when converting the Files");
+    await browser.close();
+  }
+
   try {
     const pdf = await page.pdf({
       format: "a3",
     });
-    window.showSaveDialog({ filters: { pdf: ["pdf"] } }).then((fileInfo) => {
-      try {
-        writeFileSync(fileInfo!.fsPath, pdf);
-      } catch (e) {
-        console.log(e);
-        window.showErrorMessage("Something went wrong when saving the Files");
-      }
-    });
+    window
+      .showSaveDialog({ filters: { pdf: ["pdf"] } })
+      .then(async (fileInfo) => {
+        try {
+          writeFileSync(fileInfo!.fsPath, pdf);
+        } catch (e) {
+          console.log(e);
+          window.showErrorMessage("Something went wrong when saving the Files");
+          await browser.close();
+        }
+      });
   } catch (e) {
     console.log(e);
     window.showErrorMessage("Something went wrong when converting the Files");
+    browser.process()?.kill("SIGINT");
+    await browser.close();
   }
-
-  browser.close();
+  browser.process()?.kill("SIGINT");
+  await browser.close();
   return;
 }
---
+++ src/utils/getOSSpecificPath.ts
@@ -0,0 +1,40 @@
+import { normalize } from "path";
+import { exec } from "child_process";
+import { promisify } from "util";
+const execPromise = promisify(exec);
+
+export async function getOSSpecificPath() {
+  const command = "";
+  switch (process.platform) {
+    case "win32":
+      return await getPathWindows();
+      break;
+    case "darwin":
+      break;
+    default:
+      break;
+  }
+}
+
+async function getPathWindows() {
+  const normQuery = normalize(
+    "HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths"
+  );
+  const { stdout } = await execPromise(`reg QUERY "${normQuery}" /s`);
+  const chunks = [];
+
+  for await (let chunk of stdout!) {
+    chunks.push(Buffer.from(chunk));
+  }
+
+  const buffer = Buffer.concat(chunks);
+  const str = buffer.toString("utf-8");
+  let browser: string | undefined = "";
+  browser = str.match(/\s*(Standard).*chrome.exe/)
+    ? str.match(/\s*(Standard).*chrome.exe/)![0]
+    : str.match(/\s*(Standard).*firefox.exe/)![0];
+  if (!browser) return undefined;
+
+  const path = browser.trim().substr(23);
+  return path;
+}
